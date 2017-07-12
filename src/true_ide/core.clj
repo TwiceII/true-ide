da78a0b9f526baebc97e8e78578b90868253664c
Twice_ii <alish1309@rambler.ru>
2017-07-12 20:09:34 +0600

    source code for simple calc

--- src/true_ide/core.clj
+++ src/true_ide/core.clj
@@ -1,6 +1,19 @@
 (ns true-ide.core
   (:gen-class))
 
+
+(defn get-op
+  [m]
+  (if (= (:op m) :add)
+    +
+    -))
+
+
+(defn calc
+  [x y m]
+  ((get-op m) x y))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
